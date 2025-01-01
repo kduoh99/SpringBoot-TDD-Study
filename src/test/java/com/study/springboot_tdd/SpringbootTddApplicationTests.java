e9c21aa2b0cc41f1b41474520122a7c8acf75212
Kang Duoh <kduoh99@gmail.com>
2025-01-01 23:55:54 +0900

    Chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/study/springboot_tdd/SpringbootTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
