710678ce4d73b26283ed82c054e39750f0eede70
Kang Duoh <kduoh99@gmail.com>
2025-01-02 15:07:53 +0900

    Test: DemoUtils 사용자 이름 정의

--- src/test/java/com/study/springboot_tdd/DemoUtilsTest.java
+++ src/test/java/com/study/springboot_tdd/DemoUtilsTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DemoUtilsTest {
 
 	DemoUtils demoUtils;
@@ -15,38 +19,19 @@ class DemoUtilsTest {
 	@BeforeEach
 	void setupBeforeEach() {
 		demoUtils = new DemoUtils();
-		System.out.println("@BeforeEach executes before the execution of each test method");
-	}
-
-	@AfterEach
-	void tearDownAfterEach() {
-		System.out.println("Running @AfterEach");
-		System.out.println();
-	}
-
-	@BeforeAll
-	static void setupBeforeEachClass() {
-		System.out.println("@BeforeAll executes only once before all test methods execution in the class");
-	}
-
-	@AfterAll
-	static void tearDownAfterAll() {
-		System.out.println("@AfterAll executes only once after all test methods execution in the class");
 	}
 
 	@Test
+	@DisplayName("Equals and Not Equals")
 	void testEqualsAndNotEquals() {
-
-		System.out.println("Running test: testEqualsAndNotEquals");
 
 		assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
 		assertNotEquals(8, demoUtils.add(1, 9), "1+9 must not be 8");
 	}
 
 	@Test
+	@DisplayName("Null and Not Null")
 	void testNullAndNotNull() {
-
-		System.out.println("Running test: testNullAndNotNull");
 
 		String str1 = null;
 		String str2 = "duoh";
@@ -54,4 +39,20 @@ class DemoUtilsTest {
 		assertNull(demoUtils.checkNull(str1), "Object should be null");
 		assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
 	}
+
+	// @AfterEach
+	// void tearDownAfterEach() {
+	// 	System.out.println("Running @AfterEach");
+	// 	System.out.println();
+	// }
+	//
+	// @BeforeAll
+	// static void setupBeforeEachClass() {
+	// 	System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+	// }
+	//
+	// @AfterAll
+	// static void tearDownAfterAll() {
+	// 	System.out.println("@AfterAll executes only once after all test methods execution in the class");
+	// }
 }
