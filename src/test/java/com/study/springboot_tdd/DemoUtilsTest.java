e6bf8a5ef526435534d2c57a9a367095a0da5eb2
Kang Duoh <kduoh99@gmail.com>
2025-01-02 15:12:47 +0900

    Test: DemoUtils Same/NotSame 및 True/False 검사 추가

--- src/test/java/com/study/springboot_tdd/DemoUtilsTest.java
+++ src/test/java/com/study/springboot_tdd/DemoUtilsTest.java
@@ -5,13 +5,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DemoUtilsTest {
 
 	DemoUtils demoUtils;
@@ -38,6 +35,27 @@ class DemoUtilsTest {
 
 		assertNull(demoUtils.checkNull(str1), "Object should be null");
 		assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+	}
+
+	@Test
+	@DisplayName("Same and Not Same")
+	void testSameAndNotSame() {
+
+		String str = "duoh";
+
+		assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to same object");
+		assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer same object");
+	}
+
+	@Test
+	@DisplayName("True and False")
+	void testTrueFalse() {
+
+		int gradeOne = 10;
+		int gradeTwo = 5;
+
+		assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+		assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
 	}
 
 	// @AfterEach
