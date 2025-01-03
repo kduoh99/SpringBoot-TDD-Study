81287941ba9d4bab0567e59f7450840dbf180644
Kang Duoh <kduoh99@gmail.com>
2025-01-04 02:00:51 +0900

    Test: FizzBuzz 데이터 파일 기반 검사 추가

--- src/test/java/com/study/tdd/FizzBuzzTest.java
+++ src/test/java/com/study/tdd/FizzBuzzTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
@@ -53,5 +55,32 @@ class FizzBuzzTest {
 		String expected = "1";
 
 		assertEquals(expected, FizzBuzz.compute(1), "Should return 1");
+	}
+
+	@ParameterizedTest(name = "value={0}, expected={1}")
+	@DisplayName("Testing with Small data file")
+	@CsvFileSource(resources = "/small-test-data.csv")
+	@Order(5)
+	void testSmallDataFile(int value, String expected) {
+
+		assertEquals(expected, FizzBuzz.compute(value));
+	}
+
+	@ParameterizedTest(name = "value={0}, expected={1}")
+	@DisplayName("Testing with Medium data file")
+	@CsvFileSource(resources = "/medium-test-data.csv")
+	@Order(6)
+	void testMediumDataFile(int value, String expected) {
+
+		assertEquals(expected, FizzBuzz.compute(value));
+	}
+
+	@ParameterizedTest(name = "value={0}, expected={1}")
+	@DisplayName("Testing with Large data file")
+	@CsvFileSource(resources = "/large-test-data.csv")
+	@Order(7)
+	void testLargeDataFile(int value, String expected) {
+
+		assertEquals(expected, FizzBuzz.compute(value));
 	}
 }
