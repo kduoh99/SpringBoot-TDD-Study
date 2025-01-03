90d55e58a3dc243de79dca1b630b1252744f86ef
Kang Duoh <kduoh99@gmail.com>
2025-01-03 17:16:23 +0900

    Test: Conditional JRE 실행 조건 추가

--- src/test/java/com/study/springboot_tdd/ConditionalTest.java
+++ src/test/java/com/study/springboot_tdd/ConditionalTest.java
@@ -2,7 +2,10 @@ package com.study.springboot_tdd;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
 
 class ConditionalTest {
@@ -34,6 +37,30 @@ class ConditionalTest {
 	@Test
 	@EnabledOnOs(OS.LINUX)
 	void testForLinuxOnly() {
+		// execute method and perform asserts
+	}
+
+	@Test
+	@EnabledOnJre(JRE.JAVA_17)
+	void testOnlyForJava17() {
+		// execute method and perform asserts
+	}
+
+	@Test
+	@EnabledOnJre(JRE.JAVA_21)
+	void testOnlyForJava21() {
+		// execute method and perform asserts
+	}
+
+	@Test
+	@EnabledForJreRange(min=JRE.JAVA_13, max=JRE.JAVA_21)
+	void testOnlyForJavaRange() {
+		// execute method and perform asserts
+	}
+
+	@Test
+	@EnabledForJreRange(min=JRE.JAVA_11)
+	void testOnlyForJavaRangeMin() {
 		// execute method and perform asserts
 	}
 }
